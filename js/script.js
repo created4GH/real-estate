65fd6d523f724c532f7e493071cfbaffe90909b5
created4gitHub <koretskyi10@gmail.com>
2022-10-31 01:56:54 +0200

    fixed

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// const swiper = new Swiper('.slider-main-block', {
+//     loop: true,
+//     navigation: {
+//         nextEl: '.body-main-block__arrow.swiper-button-next',
+//         prevEl: '.body-main-block__arrow.swiper-button-prev',
+//     }
+// });
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-btn-next',
+        prevEl: '.swiper-btn-prev',
+    },
+});
